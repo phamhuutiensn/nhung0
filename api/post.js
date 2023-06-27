d7674f0d9bac7775ca9cfb6af315a45b737212b8
phamhuutiensn <117017863+phamhuutiensn@users.noreply.github.com>
2023-06-27 12:23:40 +0700

    Update post.js

--- api/post.js
+++ api/post.js
@@ -7,7 +7,7 @@ const router = express.Router();
  *
  * @return product list | empty.
  */
-var rootURL = "https://gocthongtin.com/";
+var rootURL = "https://newstoday123.com/";
 
 var adapterForwtf111 = (function() {
   var url = require('url'),
